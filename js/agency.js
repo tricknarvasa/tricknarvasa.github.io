a86d2327a1b30bbe7256fd1439d71d322f951b62
Patrick James D. Narvasa <narvasa_patrick@yahoo.com.ph>
2018-07-04 15:24:45 +0800

    wqqw
    wqw

--- js/agency.js
+++ js/agency.js
@@ -48,3 +48,18 @@
   })
 
 })(jQuery); // End of use strict
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
